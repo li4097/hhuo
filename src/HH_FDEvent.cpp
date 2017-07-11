62853fb8f2512171aeec260919f96d2e27da54c3
刘茂 <mao.liu@baidao.com>
2017-07-11 18:34:42 +0800

    1. 添加socket read代码；

--- src/HH_FDEvent.cpp
+++ src/HH_FDEvent.cpp
@@ -17,6 +17,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "HH_FDEvent.h"
+#include "HH_Poller.h"
 #include "HH_CircularBuffer.h"
 
 hhou::HHFDEvent::HHFDEvent(HHPoller *poller, size_t bufSize)
@@ -35,7 +36,35 @@ hhou::HHFDEvent::~HHFDEvent()
 void hhou::HHFDEvent::OnRead()
 {
     int n = (int)m_bufIn.Space();
-    char buf[n + 1];
+    char buf[TCP_BUFSIZE];
+
+    int ret = 1;
+    while (ret)
+    {
+        n = recv(handler, buf, (n < TCP_BUFSIZE) ? n : TCP_BUFSIZE, MSG_PEEK);
+        if (n < 0)
+        {
+            /// 由于是非阻塞的模式,
+            /// 所以当errno为EAGAIN时,表示当前缓冲区已无数据可读
+            if(errno == EAGAIN)
+                break;
+        }
+        else if (n == 0) /// 这里表示对端的socket已正常关闭.
+        {
+            cout << "fdEvent " << handler << " read erron" << errno << endl;
+            eventInfo.status = Close;
+            m_pPoller->DelEvent(this);
+            closesocket(handler);
+        }
+        else
+        {
+            /// TODO<添加处理代码>
+        }
+        if(n == TCP_BUFSIZE)
+            ret = 1;
+        else
+            ret = 0;
+    }
 }
 
 void hhou::HHFDEvent::OnWrite()
