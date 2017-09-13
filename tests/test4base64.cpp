55632e54276d37e0539e8f1d0bfff39f8df15d23
liumao <loveyou12300liumao@163.com>
2017-09-13 16:55:44 +0800

    fix code

--- tests/test4base64.cpp
+++ tests/test4base64.cpp
@@ -3,9 +3,9 @@
 
 int main(int argc, char *argv[])
 {
-    char *shit = "shit111";  
+    string shit = "shit111";  
     char result[1000] = {0}; /// 编码的结果  
-    hhou::Base64Encode(shit, strlen(shit), result);  
+    hhou::Base64Encode(shit.c_str(), shit.length(), result);  
     LOG(INFO) << result;  
     
 	char org[1000] = {0};    /// 解码的结果
