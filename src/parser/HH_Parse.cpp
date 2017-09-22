c0f4e45a5cfa31bf0ce8bf2f0a094a874f764b71
liumao <loveyou12300liumao@163.com>
2017-09-22 19:34:51 +0800

    bug

--- src/parser/HH_Parse.cpp
+++ src/parser/HH_Parse.cpp
@@ -16,7 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#include "../../include/HH_Parse.h"
+#include "../../../include/HH_Parse.h"
 
 bool hhou::HHParse::ParseData(void *buf, int nLen)
 {
